3b422ce1130dad912dcf2c4f268d2e3cd75ca175
softdevomar <omorfarukqqq@gmail.com>
2024-03-23 02:57:15 -0700

    Initial Commit

---
+++ OnboardingScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingScreen
+//
+//  Created by Md omor on 23/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Md omor on 23/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
