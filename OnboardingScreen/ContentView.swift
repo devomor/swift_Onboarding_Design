aaecb6dbc7688b5b83224110ccb2d5e5c4bb8ba4
softdevomar <omorfarukqqq@gmail.com>
2024-03-23 04:37:18 -0700

    ui done and click done

--- OnboardingScreen/ContentView.swift
+++ OnboardingScreen/ContentView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("currentPage") var currentPage = 1
     var body: some View {
-        OnboardingScreen()
+        
+        if currentPage > totalPage {
+            Home()
+        }
+        else{
+            OnboardingScreen()
+        }
+      
         
     }
 }
@@ -27,57 +35,85 @@ struct Home :View {
 }
 
 
+var totalPage = 3;
 //Onboading Screen
 struct OnboardingScreen :View {
+    @AppStorage("currentPage") var currentPage = 1
     var body: some View {
         ZStack{
-            VStack{
-                HStack{
-                    Text("Hello Foodies").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        .fontWeight(.semibold)
-                        .kerning(1.2)
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Skip").fontWeight(.semibold).kerning(1.2)
-                    })
-                    
-                }.padding().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
-                
-                Image("food2").resizable().aspectRatio(contentMode: .fit)
-                    .padding(.horizontal,10)
-                    .frame(height: 400)
-                Spacer(minLength: 0)
-                
-                Text("Order Food").font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            if currentPage == 1{
+                ScreenView(image:"food1", title:"Order Food", details: "ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley")
+            }
+            if currentPage == 2{
+                ScreenView(image:"food2", title:"payment method", details: "ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley")
+            }
+            if currentPage == 3{
+                ScreenView(image:"food3", title:"Confirm Order", details: "ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley")
+            }
+            
+        }
+    }
+}
+
+struct ScreenView: View {
+    @AppStorage("currentPage") var currentPage = 1
+    var image: String
+    var title: String
+    var details: String
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Hello Foodies").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
                     .kerning(1.2)
-                    .padding(.top)
-                    .padding(.bottom,5)
-                    .foregroundColor(Color("Color"))
-                
-                
-                Text("ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It h").font(.body).fontWeight(.regular)
-                    .kerning(1.2)
-                    .padding([.leading, .trailing])
-                    .multilineTextAlignment(.center)
-                
-                Spacer(minLength: 30)
-                
-                Button(action: {}, label:{
-                    Text("Next")
-                        .fontWeight(.semibold)
-                        .frame(minWidth: 0,maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color("Color"))
-                        .cornerRadius(40)
-                        .padding(.horizontal,16)
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Skip").fontWeight(.semibold).kerning(1.2)
                 })
                 
-                
-                
-            }
+            }.padding().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            
+            Image(image).resizable().aspectRatio(contentMode: .fit)
+                .padding(.horizontal,10)
+                .frame(height: 400)
+            Spacer(minLength: 0)
+            
+            Text(title).font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .kerning(1.2)
+                .padding(.top)
+                .padding(.bottom,5)
+                .foregroundColor(Color("Color"))
+            
+            
+            Text(details).font(.body).fontWeight(.regular)
+                .kerning(1.2)
+                .padding([.leading, .trailing])
+                .multilineTextAlignment(.center)
+            
+            Spacer(minLength: 30)
+            
+            Button(action: {
+                if currentPage <= totalPage{
+                    currentPage += 1
+                }
+                else{
+                    currentPage = 1
+                }
+            }, label:{
+                Text("Next")
+                    .fontWeight(.semibold)
+                    .frame(minWidth: 0,maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color("Color"))
+                    .cornerRadius(40)
+                    .padding(.horizontal,16)
+            })
+            
+            
+            
         }
     }
 }
